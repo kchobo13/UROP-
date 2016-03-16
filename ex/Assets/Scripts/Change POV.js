1c69c7e6de61833a39444e8feac7091237c343f5
Kevin Cho <kcho13@mit.edu>
2016-03-16 14:32:46 -0400

    two cameras

---
+++ ex/Assets/Scripts/Change POV.js
@@ -0,0 +1,28 @@
+﻿#pragma strict
+
+var Player : GameObject;
+var FPCam : GameObject;
+var TPCam : GameObject;
+var check;
+
+
+function Start () {
+	FPCam.gameObject.active = false;
+	TPCam.gameObject.active = true;
+	check = true;
+}
+
+function Update () {
+	if (Input.GetKeyDown(KeyCode.R)) {
+		if (check) {
+			FPCam.gameObject.active = true;
+			TPCam.gameObject.active = false;
+		}
+		else {
+			FPCam.gameObject.active = false;
+			TPCam.gameObject.active = true;
+		}
+
+		check = !check;
+	}
+}
