2926c71bc3ea258a1db1b8763dd355b6aba46b5c
Kevin Cho <kcho13@mit.edu>
2016-03-30 17:34:05 -0400

    changes to game

---
+++ ex/Assets/Scripts/Camera.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Camera : MonoBehaviour {
+	public Transform target;
+	public float smoothTime = 5f;
+	public float maxSpeed = 10f;
+	Vector3 toTarget;
+	Vector3 velocity;
+
+	// Use this for initialization
+	void Start () {
+		toTarget = target.position - this.transform.position;
+	}
+
+
+	void FixedUpdate () {
+		if (Input.GetKeyDown (KeyCode.R)) 
+		{
+			var newPosition = Vector3.SmoothDamp (this.transform.position, target.position - toTarget, ref velocity, smoothTime, maxSpeed);
+			this.transform.position = newPosition;
+		}
+	}
+}
--- ex/Assets/Scripts/PlayerController.cs
+++ ex/Assets/Scripts/PlayerController.cs
@@ -1,23 +1,39 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour {
 
     private Rigidbody rb;
-    public float speed;
+    public float speed = 0.04f;
+	public float clockwise = 150.0f;
+	public float counterClockwise = -150.0f;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+
     }
+
+	void Restart() 
+	{
+		SceneManager.LoadScene ("minigame");
+	}
+
+	void Update()
+	{
+		rb.position += transform.forward * speed;
+	}
+
 
     void FixedUpdate()
     {
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
-
-        Vector3 movement = new Vector3(moveHorizontal, 0, moveVertical);
-        rb.AddForce(movement * speed);
+		if(Input.GetKey(KeyCode.Q)) {
+			transform.Rotate(0, Time.deltaTime * counterClockwise, 0);
+		}
+		else if(Input.GetKey(KeyCode.E)) {
+			transform.Rotate(0, Time.deltaTime * clockwise, 0);
+		}
     }
 
     void OnTriggerEnter(Collider other)
@@ -26,5 +42,9 @@ public class PlayerController : MonoBehaviour {
         {
             other.gameObject.SetActive(false);
         }
+		if (other.gameObject.CompareTag("die"))
+		{	
+			Restart ();
+		}
     }
 }
