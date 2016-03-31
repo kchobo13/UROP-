ae606dab35a84331520853e17ef70ea58dea3936
kchobo13 <kcho13@mit.edu>
2016-03-31 16:58:16 -0400

    Camera switch is now working

--- ex/Assets/Scripts/Camera.cs
+++ ex/Assets/Scripts/Camera.cs
@@ -2,23 +2,17 @@
 using System.Collections;
 
 public class Camera : MonoBehaviour {
-	public Transform target;
-	public float smoothTime = 5f;
-	public float maxSpeed = 10f;
-	Vector3 toTarget;
-	Vector3 velocity;
+    public GameObject player;
 
 	// Use this for initialization
 	void Start () {
-		toTarget = target.position - this.transform.position;
 	}
 
 
 	void FixedUpdate () {
 		if (Input.GetKeyDown (KeyCode.R)) 
 		{
-			var newPosition = Vector3.SmoothDamp (this.transform.position, target.position - toTarget, ref velocity, smoothTime, maxSpeed);
-			this.transform.position = newPosition;
+            this.transform.position = Vector3.MoveTowards(this.transform.position, player.transform.position, 1.0f);
 		}
 	}
 }
---
+++ ex/Assets/Scripts/Interpolate.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Interpolate : MonoBehaviour
+{
+
+    public GameObject player;
+    public GameObject pointA;
+    public float speed;
+    public float smoothTime;
+    public bool in_room = true;
+    public bool switch_cam;
+    bool start_smooth;
+    public float error;
+
+    Vector3 velocity = Vector3.zero;
+
+    void Start()
+    {
+        pointA.transform.position = transform.position;
+    }
+
+    void Update()
+    {
+        if (switch_cam)
+        {
+            if (!in_room)
+            {
+                start_smooth = true;
+                if (start_smooth)
+                {
+                    transform.position = Vector3.SmoothDamp(transform.position,
+                                                            pointA.transform.position,
+                                                            ref velocity,
+                                                            smoothTime);
+                    transform.rotation = Quaternion.SlerpUnclamped(transform.rotation,
+                                                                   pointA.transform.rotation,
+                                                                   Time.deltaTime * speed);
+                    if ((transform.position - pointA.transform.position).magnitude <= error)
+                    {
+                        start_smooth = !start_smooth;
+                        in_room = !in_room;
+                        switch_cam = !switch_cam;
+                    }
+                }
+            }
+            else
+            {
+                start_smooth = true;
+                if (start_smooth)
+                {
+                    transform.position = Vector3.SmoothDamp(transform.position,
+                                                            player.transform.position,
+                                                            ref velocity,
+                                                            smoothTime);
+                    transform.rotation = Quaternion.SlerpUnclamped(transform.rotation,
+                                                                   player.transform.rotation,
+                                                                   Time.deltaTime * speed);
+
+                    if ((transform.position - player.transform.position).magnitude <= error)
+                    {
+                        start_smooth = !start_smooth;
+                        in_room = !in_room;
+                        switch_cam = !switch_cam;
+                    }
+                }
+            }
+        }
+    }
+}
